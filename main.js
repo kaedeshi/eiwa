429cc7608617d8e49af9cd1ccff38f15a22049d3
kaedeshi <149069128+kaedeshi@users.noreply.github.com>
2024-04-20 00:45:18 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -48,132 +48,126 @@
   const target9 = document.querySelector('main .service .service-example>div .example3');
   const target10 = document.querySelector('main .recruit .recruit-content .recruit-img');
 
-  window.addEventListener('scroll',() =>{
+  window.addEventListener('scroll', () => {
     let targetPosition = window.innerHeight * 2 / 3;
     let targetItemPosition1 = target1.getBoundingClientRect().top;
 
-    if(targetItemPosition1 < targetPosition) {
+    if (targetItemPosition1 < targetPosition) {
       target1.style.opacity = '1';
       target1.style.transform = 'translateY(0)';
     }
   });
 
-  window.addEventListener('scroll',() =>{
+  window.addEventListener('scroll', () => {
     let targetPosition = window.innerHeight * 2 / 3;
     let targetItemPosition2 = target2.getBoundingClientRect().top;
 
-    if(targetItemPosition2 < targetPosition) {
+    if (targetItemPosition2 < targetPosition) {
       target2.style.opacity = '1';
       target2.style.transform = 'translateY(0)';
     }
   });
 
-  window.addEventListener('scroll',() =>{
+  window.addEventListener('scroll', () => {
     let targetPosition = window.innerHeight * 2 / 3;
     let targetItemPosition3 = target3.getBoundingClientRect().top;
 
-    if(targetItemPosition3 < targetPosition) {
+    if (targetItemPosition3 < targetPosition) {
       target3.style.opacity = '1';
     }
   });
 
-  window.addEventListener('scroll',() =>{
+  window.addEventListener('scroll', () => {
     let targetPosition = window.innerHeight * 2 / 3;
     let targetItemPosition3 = target4.getBoundingClientRect().top;
 
-    if(targetItemPosition3 < targetPosition) {
+    if (targetItemPosition3 < targetPosition) {
       target4.style.opacity = '1';
     }
   });
 
-  window.addEventListener('scroll',() =>{
+  window.addEventListener('scroll', () => {
     let targetPosition = window.innerHeight * 2 / 3;
     let targetItemPosition3 = target5.getBoundingClientRect().top;
 
-    if(targetItemPosition3 < targetPosition) {
+    if (targetItemPosition3 < targetPosition) {
       target5.style.opacity = '1';
     }
   });
 
-  window.addEventListener('scroll',() =>{
+  window.addEventListener('scroll', () => {
     let targetPosition = window.innerHeight * 2 / 3;
     let targetItemPosition3 = target6.getBoundingClientRect().top;
 
-    if(targetItemPosition3 < targetPosition) {
+    if (targetItemPosition3 < targetPosition) {
       target6.style.opacity = '1';
     }
   });
 
-  window.addEventListener('scroll',() =>{
+  window.addEventListener('scroll', () => {
     let targetPosition = window.innerHeight * 2 / 3;
     let targetItemPosition3 = target7.getBoundingClientRect().top;
 
-    if(targetItemPosition3 < targetPosition) {
+    if (targetItemPosition3 < targetPosition) {
       target7.style.opacity = '1';
     }
   });
 
-  window.addEventListener('scroll',() =>{
+  window.addEventListener('scroll', () => {
     let targetPosition = window.innerHeight * 2 / 3;
     let targetItemPosition3 = target8.getBoundingClientRect().top;
 
-    if(targetItemPosition3 < targetPosition) {
+    if (targetItemPosition3 < targetPosition) {
       target8.style.opacity = '1';
     }
   });
 
-  window.addEventListener('scroll',() =>{
+  window.addEventListener('scroll', () => {
     let targetPosition = window.innerHeight * 2 / 3;
     let targetItemPosition3 = target9.getBoundingClientRect().top;
 
-    if(targetItemPosition3 < targetPosition) {
+    if (targetItemPosition3 < targetPosition) {
       target9.style.opacity = '1';
     }
   });
 
-  window.addEventListener('scroll',() =>{
+  window.addEventListener('scroll', () => {
     let targetPosition = window.innerHeight * 2 / 3;
     let targetItemPosition3 = target10.getBoundingClientRect().top;
 
-    if(targetItemPosition3 < targetPosition) {
+    if (targetItemPosition3 < targetPosition) {
       target10.style.opacity = '1';
       target10.style.transform = 'translateY(0)';
     }
   });
 
 
-  // const check1 = document.getElementById('check1');
-  // const check2 = document.getElementById('check2');
-  // const check3 = document.getElementById('check3');
-  // const check4 = document.getElementById('check4');
-  // const check5 = document.getElementById('check5');
-  // const check6 = document.getElementById('check6');
-  // const check7 = document.getElementById('check7');
-  // const contactBtn = document.getElementById('contact-btn');
+  document.addEventListener('DOMContentLoaded', function () {
+    const contactBtn = document.getElementById('contact-btn');
 
+    function update() {
+      const check1 = document.getElementById('check1').value.trim();
+      const check2 = document.getElementById('check2').value.trim();
+      const check3 = document.getElementById('check3').value.trim();
+      const check4 = document.getElementById('check4').value.trim();
+      const check5 = document.getElementById('check5').value.trim();
+      const check6 = document.getElementById('check6').value.trim();
 
-  // contactBtn.classList.remove('show');
-  // update();
+      if(check1 !== '-- お問い合わせ種別を選択してください --' && check2 !== '' && check3 !== '' && check4 !== '' && check5 !== '' && check6 !== '' && document.getElementById('check7').checked && check3 === check4) {
+        contactBtn.classList.add('show');
+      } else {
+        contactBtn.classList.remove('show');
+      }
+    }
 
-  // check1.addEventListener("input", update);
-  // check2.addEventListener("input", update);
-  // check3.addEventListener("input", update);
-  // check4.addEventListener("input", update);
-  // check5.addEventListener("input", update);
-  // check6.addEventListener("input", update);
-  // check7.addEventListener("change", update);
+    check1.addEventListener("input", update);
+    check2.addEventListener("input", update);
+    check3.addEventListener("input", update);
+    check4.addEventListener("input", update);
+    check5.addEventListener("input", update);
+    check6.addEventListener("input", update);
+    check7.addEventListener("change", update);
 
-  // function update() {
-  //   let isRequired2 = check2.checkVisibility();
-  //   let isRequired3 = check3.checkVisibility();
-  //   let isRequired4 = check4.checkVisibility();
-  //   let isRequired5 = check5.checkVisibility();
-  //   let isRequired6 = check6.checkVisibility();
-  //   let isRequired7 = check7.checkVisibility();
-
-  //   if(check1.value !== '-- お問い合わせ種別を選択してください --' && isRequired2  && isRequired3  && isRequired4  && isRequired5  && isRequired6  && isRequired7) {
-  //     contactBtn.classList.add('show');
-  //   }
-  // }
-
+    update();
+  });
 }
